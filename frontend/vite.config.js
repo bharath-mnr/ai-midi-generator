57b52e15c2897728bfb9e2c777217daad78cccc5
bharath-mnr <bharathmnr@outlook.com>
2025-10-05 08:29:51 +0530

    remove add tailwind CSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [ tailwindcss(),react()],
-// })
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite' // Comment this out
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Remove tailwindcss() temporarily
+  plugins: [ tailwindcss(),react()],
 })
